0bde93205895f9f244beff3dfde2bd4ab66f7783
Yuki Nishijima <yuki@xpillars.software>
2025-01-10 17:24:55 +0900

    Colorize the test output

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,7 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "active_support/test_case"
 require "active_support/testing/autorun"
 
+require "minitest/pride"
 require "rails/version"
 require "rails"
 require "rails/railtie"
